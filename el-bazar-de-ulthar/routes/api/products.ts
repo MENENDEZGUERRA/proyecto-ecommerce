b604d5fac4b99eb570eb4aa293d3e1c2c23673b2
MENENDEZGUERRA <menendezguerrajuan@gmail.com>
2025-06-02 20:41:42 -0600

    Added photos to products

--- el-bazar-de-ulthar/routes/api/products.ts
+++ el-bazar-de-ulthar/routes/api/products.ts
@@ -7,7 +7,7 @@ export const products = [
     "description": "Un espejo que muestra tu reflejo envejecido 10 años",
     "price": 250.00,
     "discountPercentage": 20,
-    "imageUrl": "/media/products/mirror.jpg"
+    "imageUrl": "https://cdn.pixabay.com/photo/2019/06/03/02/54/skull-4248008_1280.jpg"
   },
   {
     "id": 2,
@@ -15,7 +15,7 @@ export const products = [
     "description": "Una cámara que toma fotos del pasado",
     "price": 150.00,
     "discountPercentage": 10,
-    "imageUrl": "/media/products/camera.jpg"
+    "imageUrl": "https://cdn.pixabay.com/photo/2016/04/27/12/26/photo-camera-1356510_1280.jpg"
   },
   {
     "id": 3,
@@ -23,7 +23,7 @@ export const products = [
     "description": "Un reloj que cuenta el tiempo hacia atrás",
     "price": 50.00,
     "discountPercentage": 5,
-    "imageUrl": "/media/products/hourglass.jpg"
+    "imageUrl": "https://images.pexels.com/photos/7703268/pexels-photo-7703268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     "id": 4,
@@ -31,15 +31,15 @@ export const products = [
     "description": "La mítica piedra que todo lo puede",
     "price": 1000.00,
     "discountPercentage": 15,
-    "imageUrl": "/media/products/philosopher_stone.jpg"
+    "imageUrl": "https://cdn.pixabay.com/photo/2023/05/23/08/13/crystal-8012176_1280.jpg"
   },
   {
     "id": 5,
     "name": "Varita Mágica",
     "description": "Una varita que concede tres deseos",
     "price": 300.00,
-    "discountPercentage": 25,
-    "imageUrl": "/media/products/magic_wand.jpg"
+    "discountPercentage": 0,
+    "imageUrl": "https://cdn.pixabay.com/photo/2019/09/26/12/37/fabric-4505840_1280.jpg"
   },
   {
     "id": 6,
@@ -47,7 +47,7 @@ export const products = [
     "description": "Una capa que te hace invisible",
     "price": 450.00,
     "discountPercentage": 30,
-    "imageUrl": "/media/products/invisibility_cloak.jpg"
+    "imageUrl": "https://statics.memondo.com/p/99/crs/2014/10/CR_929411_97882ba824094ff7ac8ca7540198d02d_mi_disfraz_de_vampiro_thumb_fb.jpg?cb=8396995"
   },
   {
     "id": 7,
@@ -55,7 +55,7 @@ export const products = [
     "description": "Un anillo que otorga habilidades sobrehumanas",
     "price": 750.00,
     "discountPercentage": 20,
-    "imageUrl": "/media/products/power_ring.jpg"
+    "imageUrl": "https://cdn.pixabay.com/photo/2017/08/13/09/25/glass-and-wood-ring-2636613_960_720.jpg"
   },
   {
     "id": 8,
@@ -63,7 +63,7 @@ export const products = [
     "description": "Un escudo que te protege de todo daño",
     "price": 600.00,
     "discountPercentage": 10,
-    "imageUrl": "/media/products/protective_shield.jpg"
+    "imageUrl": "https://images.steamusercontent.com/ugc/755968118228127437/FD17AFF113F82F60EFAF3F56EC8B3D32C6C239EB/?imw=268&imh=268&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
   },
   {
     "id": 9,
@@ -71,7 +71,7 @@ export const products = [
     "description": "Una botella que contiene elixires de la eterna juventud",
     "price": 900.00,
     "discountPercentage": 5,
-    "imageUrl": "/media/products/bottle_of_eternity.jpg"
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO2p1xY0XRj6oX1cGszHyQzezmHbZT3cM88Q&s"
   },
   {
     "id": 10,
@@ -79,7 +79,7 @@ export const products = [
     "description": "Un mapa que lleva a un tesoro escondido",
     "price": 350.00,
     "discountPercentage": 15,
-    "imageUrl": "/media/products/treasure_map.jpg"
+    "imageUrl": "https://aprende.guatemala.com/wp-content/uploads/2017/03/El-mapa-de-Guatemala-y-sus-cambios-a-través-del-tiempo.jpg"
   }
 ];
 
